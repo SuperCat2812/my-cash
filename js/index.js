27787d41ac051d8bef252561205592852e2021e2
SuperCat2812git config --global user.name SuperCat2812 <Sashka281205@gmail.com>
2022-09-28 19:33:13 +0200

    format

--- js/index.js
+++ js/index.js
@@ -1,28 +1,27 @@
 let n1 = document.getElementById("n1");
 let p = document.getElementById("p");
 let b = document.getElementById("b");
-b.onclick=function() {
+b.onclick = function () {
   let premia = 0.15;
-  let premiap = +n1.value* premia;
+  let premiap = +n1.value * premia;
   let nalog = 0.1;
   let nalogp = (+n1.value + premiap) * nalog;
-  let balans = (+n1.value + premiap)-nalogp;
+  let balans = (+n1.value + premiap) - nalogp;
   let shop = 190;
-  let ost=balans - shop;
+  let ost = balans - shop;
   let prochend = ost / 2;
-  
- 
-  p.textContent="Остаток денег у пользователя"+prochend;
+
+
+  p.textContent = "Остаток денег у пользователя" + prochend;
 }
 
 let v = 6;
-for (let i = 0; i <= v; i++){
+for (let i = 0; i <= v; i++) {
   let ter = [];
-  for (let g = 0; g <= i; g++){
+  for (let g = 0; g <= i; g++) {
     ter.push("#");
-    
+
   }
-  let n=ter.join("")
+  let n = ter.join("")
   console.log(n);
 }
-
